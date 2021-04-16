634980a2f9f122f333ebefebada9fbc741afcc89
Juan Carpinteiro <juanluiscarpinteiro@users.noreply.github.com>
2021-04-16 14:30:50 -0400

    Finished the scenario for the UI test

--- features/UIAutomation.feature
+++ features/UIAutomation.feature
@@ -9,16 +9,15 @@ Feature: User should be able to load, log in and purchase all t-shirts
 	Scenario: I should be able to load the browswer get credentials and log in followed by purchase all t-shirts
         When I load the sauce demo site 
         Then I get credentials from the UI
-        # Then I enter the user name and password available
-        # And click on "login" button
-        # Then I scann for all elements present on page
-        # Then I add to cart all of the elements available on screen
-        # Then I clcik on "cart" icon
-        # Then I verify screen has the following element text "Your Cart"
-        # Then I click on "checkout" button
-        # Then I verify screen has the following element text "CHECKOUT: YOUR INFORMATION"
-        # Then I enter "Juan" as first name, "Carpinteiro" as last name, and "10583" as zipcode
-        # Then I click on "continue" button  
-        # Then I verify screen has the following element text "CHECKOUT: OVERVIEW"
-        # Then I click on "finish" button
-        # Then I verify the following message on screen: "Thank you for your order, your order has been dispatched, and will arrive just as fast as the pony can get there"
+        Then I enter the user name and password available
+        Then I click on "login" custom button
+        Then I add to cart all of the elements available on screen
+        Then I clcik on cart icon
+        Then I verify screen has the following element text "Your Cart"
+        Then I click on "checkout" button
+        Then I verify screen has the following element text "Checkout: Your Information"
+        Then I enter "Juan" as first name, "Carpinteiro" as last name, and "10583" as zipcode
+        Then I click on "continue" button  
+        Then I verify screen has the following element text "Checkout: Overview"
+        Then I click on "finish" button
+        Then I verify the following message on screen: "THANK YOU FOR YOUR ORDER"
--- features/step_definitions/uiAutomation.rb
+++ features/step_definitions/uiAutomation.rb
@@ -18,6 +18,11 @@ Then(/^I click on "([^"]*)" button$/) do |button|
   click_button(button, wait:30)
 end
 
+Then(/^I click on "([^"]*)" custom button$/) do |button|
+  page.find(:xpath, config['saucePage']['loginButton'], wait:30).click    
+end
+
+
 Then(/^I get credentials from the UI$/) do
   username= page.find(:xpath, config['saucePage']['usermane'], wait:30).text
   usernameArray = username.split(" ")
@@ -40,7 +45,35 @@ Then(/^I get credentials from the UI$/) do
 
 end
 
-Then(/^I click on "([^"]*)" button$/) do |button|
-	expect(page).to have_button(button, wait: 30)
-  click_button(button, wait:30)
+
+Then(/^I enter the user name and password available$/) do 
+	page.find(:xpath, config['saucePage']['usernamePlaceholderInput'], wait:30).set(@usernameFound)
+  page.find(:xpath, config['saucePage']['passwordPlaceholderInput'], wait:30).set(@passwordFound)
+end
+
+
+Then(/^I add to cart all of the elements available on screen$/) do 
+  availableItemsToPurchase  = page.all(:xpath, config['saucePage']['addToCartButtons'], wait:30)
+  
+  availableItemsToPurchase.each do|item|
+      item.click
+    end
+end
+
+Then(/^I clcik on cart icon$/) do 
+	page.find(:xpath, config['saucePage']['cartIcon'], wait:30).click
+end
+
+Then(/^I verify screen has the following element text "([^"]*)"$/) do |message|
+  expect(page).to have_xpath(config['saucePage']['titleHeader'].sub('HEADER', message), wait:30) 
+end
+
+Then(/^I enter "([^"]*)" as first name, "([^"]*)" as last name, and "([^"]*)" as zipcode$/) do |fname,lname,zip|
+  page.find(:xpath, config['saucePage']['infoInputGeneric'].sub('ENTERINFO',"firstName"), wait:30).set(fname)
+  page.find(:xpath, config['saucePage']['infoInputGeneric'].sub('ENTERINFO',"lastName"), wait:30).set(lname)
+  page.find(:xpath, config['saucePage']['infoInputGeneric'].sub('ENTERINFO',"postalCode"), wait:30).set(zip)
+end
+
+Then(/^I verify the following message on screen: "([^"]*)"$/) do |message|
+  expect(page).to have_xpath(config['saucePage']['thankYouMessage'].sub('MESSAGE', message), wait:30) 
 end
